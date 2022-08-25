958792f03a471dbc057643c161440bf2f35f14d8
SaitomTech <57802435+SaitomTech@users.noreply.github.com>
2022-08-25 16:36:14 +0900

    add: post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PostsModule } from './components/posts/posts.module';
+import * as path from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.join(
+        process.cwd(),
+        'src/generated/graphql/schema.gql',
+      ),
+      sortSchema: true,
+    }),
+    PostsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
